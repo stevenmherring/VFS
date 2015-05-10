fa471d349feccb7c7c6f3e7d804beb20d15962ef
Student CSE306Spring2015 <student@vl53.cs.stonybrook.edu>
2015-05-09 21:59:23 -0400

    Fixed bug with add. When a directory is listed as a file under a directory being added to the VFHV the path wasn't being reset. Resulting in the path of all following files to be incorrect. Also fixed wolf-ls.c so it can now enter ioctl.

--- lab4/wolf-ls.c
+++ lab4/wolf-ls.c
@@ -9,14 +9,26 @@
 int main(int argc, char **argv) {
   
   int ret = 0;
+  struct wolfs_ioctl_args arg;
+
+  if (argc < 2) {
+    printf("Need an argument\n");
+    // target is the file or directory to add
+    // destination is the wolfs mount point the target is added to
+    printf("Usage: wolf-ls [destination] \n");
+    return -1;
+  }
  
   int fd = open("foo", O_DIRECTORY);
   if(fd < 0) {
     printf("Failed to open the current dir\n");
     return fd;
   }
+  
+  arg.len = strlen(argv[1]);
+  arg.buf = argv[1];
 
-  ret = ioctl(fd, WOLFS_LS, NULL);
+  ret = ioctl(fd, WOLFS_LS, &arg);
 
   if(ret != 0) 
     printf("Something went wrong with the ioctl %d\n", ret);
